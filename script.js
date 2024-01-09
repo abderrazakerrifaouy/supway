bdbf3d3c886df26e9a574f05c2ae68be00a3c192
abnderrazak-errifaouy <72007631+abderrazakerrifaouy@users.noreply.github.com>
2024-01-09 16:31:42 +0100

    updat
    updat

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ block.addEventListener("animationiteration", () => {
     scor++;
 })
 
-setInterval(()=>{
+setInterval(() => {
     let playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue('left'));
     let blockrLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
     let blockrTop = parseInt(window.getComputedStyle(block).getPropertyValue('top'));
 
-    if(playerLeft == blockrLeft && blockrTop < 480 && blockrTop > 310){
-    //   alert(`gem over !!!! \n scour = ${scor}`); 
-        blockrTop = 0 ;
-        scor = 0 ; 
+    if (playerLeft == blockrLeft && blockrTop < 480 && blockrTop > 310) {
+        alert(`gem over !!!! \n scour = ${scor}`);
+        blockrTop = 0;
+        scor = 0;
     }
     score.innerHTML = `scor = ${scor}`
 })
